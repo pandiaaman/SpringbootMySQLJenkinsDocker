94f46b02c38e748ac179563987b23cbe015943b7
Aman Pandia <pandiaaman16@gmail.com>
2024-06-13 20:37:26 +0530

    first commit project initiated

---
+++ SpringSQLJenkinsDocker/src/main/java/com/springSQLJenkinsDocker/SpringSqlJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springSQLJenkinsDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqlJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqlJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ SpringSQLJenkinsDocker/src/test/java/com/springSQLJenkinsDocker/SpringSqlJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSQLJenkinsDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqlJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
